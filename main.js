30c2eb066107689c7e6adb67e719b0d73f24e7d6
Mohanraj <MeetMohanraj@gmail.com>
2021-12-04 17:55:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ Webcam.snap(function(data_uri){
 }
 console.log('ml5 version:',ml5.version);
 
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wMmcLCFGo/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LW8bMcTRe/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('modelLoaded!');
@@ -28,6 +28,37 @@ function modelLoaded(){
 function speak(){
     var synth =window.speechSynthesis;
     speak_data_1="the first prediction is "+prediction_1;
-    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    
+
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
+}
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error)
+    }
+    else{
+     console.log(results);   
+    document.getElementById("result_emotion_name2").innerHTML =results[0].label;
+    prediction_1=results[0].label;
+    speak();
+
+    if(results[0].label=="Amazing"){
+        document.getElementById("update_emoji2").innerHTML="	&#128076;";
+
+    }
+    if(results[0].label=="Best"){
+        document.getElementById("update_emoji2").innerHTML="&#128077;";
+
+    }
+    if(results[0].label=="Victory"){
+        document.getElementById("update_emoji2").innerHTML="&#9996;";
+
+}
+    }
 }
